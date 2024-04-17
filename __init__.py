01b07cd0879935048bf2ab8d45cf2b0256ae586b
Carlos Paniagua <cpaniaguam@gmail.com>
2024-04-17 18:09:38 -0400

    Update __init__.py
    Delete later

--- __init__.py
+++ __init__.py
@@ -1,4 +1,4 @@
 # tbd: add importlib thing to get package version
-from icedrift.src import cleaning
-from icedrift.src import analysis
-from icedrift.src import interpolation
+#from icedrift.src import cleaning
+#from icedrift.src import analysis
+#from icedrift.src import interpolation
