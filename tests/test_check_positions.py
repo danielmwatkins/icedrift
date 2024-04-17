485e6b4ea92ba5474cbe987339451b11c88cca6d
daniel watkins <danielmwatkins@gmail.com>
2024-04-17 10:55:28 -0400

    bugfix

--- tests/test_check_positions.py
+++ tests/test_check_positions.py
@@ -1,8 +1,9 @@
 from icedrift.cleaning import check_positions
 import pandas as pd
 import numpy as np
+import os
 
-data = pd.read_csv('test_data/IABP_2010_300234010429080.csv')
+data = pd.read_csv(os.path.join('test_data', 'IABP_2010_300234010429080.csv'))
 
 def test_check_pairs():
     comp_pairs_only = check_positions(data, pairs_only=True)
